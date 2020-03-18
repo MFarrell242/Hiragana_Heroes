c9560b723928b331974e0b279defdc5d151197e8
Justin Abreu <justinabreu92@gmail.com>
2020-03-18 18:23:14 -0500

    got the chart to display properly

--- be-a-weeb/src/App.js
+++ be-a-weeb/src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CharWrapper from "./components/CharWrapper/CharWrapper";
+import Vowels from "./components/Vowels/Vowels";
+import Kchar from "./components/Kchar/Kchar";
+import Schar from "./components/Schar/Schar";
+import Tchar from "./components/Tchar/Tchar";
+import Nchar from "./components/Nchar/Nchar";
+import Hchar from "./components/Hchar/Hchar";
+import Mchar from "./components/Mchar/Mchar";
+import Ychar from "./components/Ychar/Ychar";
+import Rchar from "./components/Rchar/Rchar";
+import Wchar from "./components/Wchar/Wchar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <CharWrapper>
+        <Wchar />
+        <Rchar />
+        <Ychar />
+        <Mchar />
+        <Hchar />
+        <Nchar />
+        <Tchar />
+        <Schar />
+        <Kchar />
+        <Vowels />
+      </CharWrapper>
     </div>
   );
 }
