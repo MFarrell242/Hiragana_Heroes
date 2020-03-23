62d16aa3cc2aebe4b5b0779cabeea1c9efe0db95
MFarrell242 <farrell.michael.w@gmail.com>
2020-03-23 18:52:55 -0500

    well, it's something

---
+++ be-a-weeb/src/components/Navbar.js
@@ -0,0 +1,13 @@
+//Navbar, at top of every page
+
+import React from 'react';
+import { BrowserRouter as Route, Router } from 'react-router-dom';
+
+export default function Navbar() {
+    return (
+        <div className='navigator topLayer'>
+            
+
+        </div>
+    )
+}
---
+++ be-a-weeb/src/components/pages/Splash.js
@@ -0,0 +1,4 @@
+//frontpage, both login screen...
+//...and "Hello, 'your name', ready to get started?"
+//I'd suggest (to myself), make this page have stats and stuff once you log in
+//Maybe something fun at the end, like a japanese pun generator?
