d2f0939ff96017cb01b20ff856adbebc51843863
MFarrell242 <farrell.michael.w@gmail.com>
2020-03-25 18:37:12 -0500

    no changes I think?

--- be-a-weeb/src/components/Navbar.js
+++ be-a-weeb/src/components/Navbar.js
@@ -1,13 +1,13 @@
 //Navbar, at top of every page
 
 import React from 'react';
-import { BrowserRouter as Route, Router } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <div className='navigator topLayer'>
-            
-
+            <a href="/" className="navLink homeLink">Home</a>
+            <a href="/Hira101" className="navLink learnLink">Learn</a>
+            <a href="/HiraQuiz" className="navLink quizLink">Test Your Knowledge</a>
         </div>
     )
 }
