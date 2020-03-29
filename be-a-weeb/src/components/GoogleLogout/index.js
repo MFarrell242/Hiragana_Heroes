1e6a558772531ee5210136889b040e3ea8d0e8c5
Rand al'Thor <alexanderhogle@gmail.com>
2020-03-28 20:22:06 -0500

    beginner code

---
+++ be-a-weeb/src/components/GoogleLogout/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { GoogleLogout } from 'react-google-login';
+
+function GoogleLogout(props) {
+    return (
+        <button {...props}></button>
+    )
+}
+
+export default GoogleLogout;
--- be-a-weeb/src/pages/Login.js
+++ be-a-weeb/src/pages/Login.js
@@ -1,34 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
+import { GoogleLogout } from "react-google-login";
+import { GoogleLogin } from "react-google-login";
 
-function Login() {
-  return (
-      <>
-    <div className="container-fluid my-auto">
-        <div className="row justify-content-center">
-        <div className="col-4">
-          <h3 className="text-center">Welcome to Hiragana Heroes</h3>
-          <p className="text-center">Create an account to start learning today!</p>
-        </div>
-      </div>
+const responseGoogle = (response) => {
+    console.log(response);
+  }
 
-      <div className="row justify-content-center">
-        <div className="col-4 d-flex justify-content-center">
-          
-            <div className="g-signin2" data-onsuccess="onSignIn"></div>
-            {/* <a href="#" onclick="signOut();">Sign out</a> */}
-          
-        </div>
-      </div>
-      <div class="row h-100">
-   <div class="col-sm-12 my-auto">
-     <div class="card card-block w-25 mx-auto">Card</div>
-   </div>
-</div>
-    </div>
+class Login extends Component {
+    state = {
+        isLogin: true
+    }
+
+    logout = () => {
+        this.setState({ isLogin: false})
+    }
     
+
+    render() {
+        console.log(this.state.isLogin);
+        return (
+            <>
+              <div className="container-fluid">
+                <div className="row justify-content-center" style={{ marginTop: 100 }}>
+                  <div className="col-4 text-center">
+                    <h3 className="">Welcome to Hiragana Heroes</h3>
+                    <p className="">Create an account to start learning today!</p>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-4 text-center"></div>
+                  <div className="col-4 text-center">
+                    {/* <div
+                      className="g-signin2"
+                      style={{ width: 100, margin: "auto" }}
+                      data-onsuccess="onSignIn"
+                    ></div> */}
+                    <div>
+                      <GoogleLogin
+                        clientId="517700874246-m90up0h8vl88da79nmh3bjo5602msrq3.apps.googleusercontent.com"
+                        buttonText="Login"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
+                        cookiePolicy={"single_host_origin"}
+                      />
+                    </div>
+                    <div>
+                      {/* <a href="#" onclick="signOut();">
+                        Sign out
+                      </a> */}
+                      <GoogleLogout
+                        clientId="517700874246-m90up0h8vl88da79nmh3bjo5602msrq3.apps.googleusercontent.com"
+                        buttonText="Logout"
+                        onLogoutSuccess={this.logout}
+                      ></GoogleLogout>
+                    </div>
+                  </div>
+                  <div className="col-4 text-center"></div>
+                </div>
+              </div>
+            </>
+          );
+    }
   
-  </>
-  );
 }
 
 export default Login;
